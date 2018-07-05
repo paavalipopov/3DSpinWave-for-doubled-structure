ba74cf62123b9092766a095c1811e884305ac6db
Pavel <kapssula@gmail.com>
2018-07-05 14:03:25 +0300

    script for running several runs

---
+++ double.bat
@@ -0,0 +1,6 @@
+@echo off
+cd C:\eigens
+D:\Projects\spinwaves3000\spinwaves3000\bin\Release\spinwaves3000.exe 10.0e-4
+D:\Projects\spinwaves3000\spinwaves3000\bin\Release\spinwaves3000.exe 2.0e-4
+D:\Projects\spinwaves3000\spinwaves3000\bin\Release\spinwaves3000.exe 0.5e-4
+pause
